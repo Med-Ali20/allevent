5fcc3906a22524f5bc01133bf2e80c26ba7ba3a9
Ahmed Ali <ahmedali@Ahmeds-MacBook-Air.local>
2025-07-15 16:34:24 +0300

    added zain font locally

--- app/font.ts
+++ app/font.ts
@@ -1,5 +1,54 @@
 // app/fonts.ts
 import { Outfit, Montserrat, Zain } from 'next/font/google';
+import localFont from 'next/font/local'
+
+export const zain = localFont({
+  src: [
+    {
+      path: '../public/fonts/Zain/Zain-Regular.ttf',
+      weight: '400',
+      style: 'normal',
+    },
+    {
+      path: '../public/fonts/Zain/Zain-Bold.ttf',
+      weight: '700',
+      style: 'normal',
+    },
+    {
+      path: '../public/fonts/Zain/Zain-Light.ttf',
+      weight: '300',
+      style: 'normal',
+    },
+    {
+      path: '../public/fonts/Zain/Zain-LightItalic.ttf',
+      weight: '300',
+      style: 'italic',
+    },
+    {
+      path: '../public/fonts/Zain/Zain-Italic.ttf',
+      weight: '400',
+      style: 'italic',
+    },
+    {
+      path: '../public/fonts/Zain/Zain-ExtraBold.ttf',
+      weight: '800',
+      style: 'normal',
+    },
+    {
+      path: '../public/fonts/Zain/Zain-ExtraLight.ttf',
+      weight: '200',
+      style: 'normal',
+    },
+    {
+      path: '../public/fonts/Zain/Zain-Black.ttf',
+      weight: '900',
+      style: 'normal',
+    },
+  ],
+  variable: '--font-zain',
+  display: 'swap',
+})
+
 
 export const outfit = Outfit({
   subsets: ['latin'],
@@ -15,9 +64,9 @@ export const montserrat = Montserrat({
   display: 'swap',
 });
 
-export const zain = Zain({
-  subsets: ['latin', 'arabic'],
-  weight: ['200', '300', '400', '700', '800', '900'],
-  variable: '--font-zain',
-  display: 'swap',
-});
+// export const zain = Zain({
+//   subsets: ['latin', 'arabic'],
+//   weight: ['200', '300', '400', '700', '800', '900'],
+//   variable: '--font-zain',
+//   display: 'swap',
+// });
